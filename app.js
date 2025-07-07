ef81cd9da2214feab0dfa00e9f6471568ffbf191
FaridaNelson <faridanelson@gmail.com>
2025-07-07 15:34:24 +0400

    Created a mock userID

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use("/", mainRouter);
 app.use("/items", clothingItemRouter);
 
