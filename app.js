61759053d530fa395e85d600f9e9a1f1317b24ea
FaridaNelson <faridanelson@gmail.com>
2025-09-07 17:51:56 -0700

    added a logger.js with requestLogger and errorLogger, imported and used in app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const { PORT = 3001 } = process.env;
 
 const { errors } = require("celebrate");
 const auth = require("./middlewares/auth");
-const errorHandler = require("./middlewares/error-handler");
+const errorHandler = require("./middlewares/errorHandler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const mainRouter = require("./routes");
 const { notFoundHandler } = require("./utils/error-codes");
 
@@ -15,6 +16,9 @@ mongoose.connect("mongodb://localhost:27017/wtwr_db");
 
 app.use(cors());
 app.use(express.json());
+
+// Request loggging middleware
+app.use(requestLogger);
 
 // no authorization required for these routes
 app.post("/signin", require("./controllers/users").login);
@@ -29,11 +33,14 @@ app.use(auth);
 // Protected routes
 app.use("/", mainRouter);
 
-// Celebrate error handler
-app.use(errors());
-
 // Error handling middleware
 app.use(notFoundHandler);
+
+// Error loggging middleware
+app.use(errorLogger);
+
+// Celebrate error handler
+app.use(errors());
 
 app.use(errorHandler);
 
