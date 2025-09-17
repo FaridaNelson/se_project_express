da0ebb884ef69c0c453e26d2ec983766d1687cb5
FaridaNelson <faridanelson@gmail.com>
2025-09-17 08:33:09 -0700

    changed error name inside auth.js for error handling consistency

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const UnauthorizedError = require("../errors/UnauthorizedError");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   // 1. Check if Authorization header exists and starts with "Bearer "
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    next(new Error("Authorization required"));
+    next(new UnauthorizedError("Authorization required"));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -16,7 +17,7 @@ module.exports = (req, res, next) => {
     // 2. Verify token
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new Error("Invalid token"));
+    next(new UnauthorizedError("Invalid token"));
   }
   // 3. Attach user payload to request
   req.user = payload;
